9515f4fb15d1109c8315d0427ee2c1464def8034
asimanj42 <asimanmammadli2023@gmail.com>
2024-02-10 00:36:39 +0400

    Project was added

---
+++ liquibase-demo/src/main/java/com/example/liquibasedemo/entity/Student.java
@@ -0,0 +1,23 @@
+package com.example.liquibasedemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private Long gpa;
+//    private String email;
+
+}
